572c037e04ff38712c7fc91722e6edf6230c9173
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-07 20:20:15 +0530

    Writing the Python code

--- grocery-app/backend/products_dao.py
+++ grocery-app/backend/products_dao.py
@@ -3,4 +3,9 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root', password='Pray@#2010',
                               host='127.0.0.1',
                               database='shopsense')
+
+cursor = cnx.cursor()
+query = "SELECT * FROM shopsense.products"
+
+cursor.execute(query)
 cnx.close()
