4d79dbe7778cdddf296d4c9c7cc923d419df3852
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-09 11:25:00 +0530

    Setting up the backend

--- grocery-app/backend/products_dao.py
+++ grocery-app/backend/products_dao.py
@@ -1,11 +1,30 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='Pray@#2010',
-                              host='127.0.0.1',
-                              database='shopsense')
 
-cursor = cnx.cursor()
-query = "SELECT * FROM shopsense.products"
+def get_all_products():
+    cnx = mysql.connector.connect(
+        user="root", password="Pray@#2010", host="127.0.0.1", database="shopsense"
+    )
 
-cursor.execute(query)
-cnx.close()
+    cursor = cnx.cursor()
+
+    query = "SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom_table.uom_name FROM shopsense.products inner join shopsense.uom_table on products.uom_id=uom_table.uom_id"
+
+    cursor.execute(query)
+
+    response = []
+
+    for product_id, name, uom_id, price_per_unit, uom_name in cursor:
+        response.append(
+            {
+                "product_id": product_id,
+                "name": name,
+                "uom_id": uom_id,
+                "price_per_unit": price_per_unit,
+                "uom_name": uom_name,
+            }
+        )
+
+    cnx.close()
+
+    return response
