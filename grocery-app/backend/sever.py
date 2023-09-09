cde6e4f379ef4b5083bce04cac150014f2d49a62
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-09 20:19:54 +0530

    Setting up the UI

--- grocery-app/backend/sever.py
+++ grocery-app/backend/sever.py
@@ -15,6 +15,14 @@ def get_products():
     return response
 
 
+@app.route("/deleteProduct", methods=["POST"])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form["product_id"])
+    response = jsonify({"product_id": return_id})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Shopsense")
     app.run(port=5000)
